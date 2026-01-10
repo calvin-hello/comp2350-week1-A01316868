8532476510c433eda56822ce2cdd3faf4ae83040
Calvin Mwin-Era <45calvin45@gmail.com>
2026-01-10 14:22:26 -0800

    added files for database lab01

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3016;
+
+app.get('/', (req, res) => {
+  res.send(`Calvin Mwin-Era! (:{) `);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
